bce40ffd50ca2dca3f93df4550b64db70a573c62
Juliette.Bornier <juliette.bornier.3110@gmail.com>
2024-07-25 17:48:12 +0200

    add colors

--- src/inflation/charts_creator.py
+++ src/inflation/charts_creator.py
@@ -1,17 +1,15 @@
+import pandas as pd
 import plotly.express as px
-import pandas as pd 
 
 
 def plot_inflation(df_country):
     df_country["original_period"] = pd.to_datetime(df_country["original_period"])
-    df_country["original_period"] = df_country["original_period"].dt.strftime(
-            "%Y/%m"
-        )
+    df_country["original_period"] = df_country["original_period"].dt.strftime("%Y/%m")
     fig = px.line(
         df_country,
         x="original_period",
         y="inflation",
-        title= "Inflation Evolution",
+        title="Inflation Evolution",
         labels={
             "original_period": "Date",
             "inflation": "Inflation ",
@@ -27,21 +25,23 @@ def plot_inflation(df_country):
                 "Date: %{customdata[0]}",
                 "Inflation (%): %{customdata[1]}",
             ]
-        )
+        ),
+        line=dict(color="deepink"),
     )
 
     return fig
 
+
 def plot_commodity(df_commodity):
     df_commodity["original_period"] = pd.to_datetime(df_commodity["original_period"])
     df_commodity["original_period"] = df_commodity["original_period"].dt.strftime(
-            "%Y/%m"
-        )
+        "%Y/%m"
+    )
     fig = px.line(
         df_commodity,
         x="original_period",
         y="commodity prices",
-        title= "Commodity Prices Evolution",
+        title="Commodity Prices Evolution",
         labels={
             "original_period": "Date",
             "commodity prices": "Commodity Price Evolution",
@@ -57,7 +57,8 @@ def plot_commodity(df_commodity):
                 "Date: %{customdata[0]}",
                 "Commodity Prices (%): %{customdata[1]}",
             ]
-        )
+        ),
+        line=dict(color="gold"),
     )
 
     return fig
